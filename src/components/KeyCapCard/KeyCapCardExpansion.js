fcf5f8b649507ef6e0181543777d173b240fe40c
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-07 13:16:12 +0000

    rearranged css

--- src/components/KeyCapCard/KeyCapCardExpansion.js
+++ src/components/KeyCapCard/KeyCapCardExpansion.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import SellerButton from './SellerButton'
+import nautCore from '../../assets/img/gmk-nautilus-core.jpg'
 
 const CardFooterExpansion = styled.div`
-  height: max-content;
-  padding: 1rem;
   background-color: ${props => props.backgroundColor};
   border-top: 1px solid white;
+  height: max-content;
+  padding: 1rem;
   cursor: initial;
 `;
 
@@ -17,10 +18,29 @@ const SellerButtonContainer = styled.div`
   padding: 0 1rem;
 `;
 
+const CoreImgContainer = styled.div`
+  background-image: url(${props => props.img});
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  height: 300px;
+  margin-bottom: 1rem;
+  width: auto;
+  @media (max-width: 750px) {
+    height: 12rem;
+  }  
+  @media (max-width: 600px) {
+    height: 10rem;
+  }
+  @media (max-width: 400px) {
+    height: 8rem;
+  }
+`;
+
 export default function KeyCapCardExpansion(props) {
   return (
     <CardFooterExpansion backgroundColor={props.backgroundColor}>
-
+      <CoreImgContainer img={nautCore} />
       <SellerButtonContainer>
         <SellerButton seller='Novel Keys' />
         <SellerButton seller='Novel Keys' />
