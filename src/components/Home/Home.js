3f6c0db77282933561aa478880f5c954b39b521c
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-05-09 11:33:36 +0000

    building out HomePage, got background picture implemented and heading

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,17 +2,56 @@ import React from 'react'
 import styled from 'styled-components'
 import homePic from '../../assets/img/home-md.jpg'
 
-const Landing = styled.div`
-  background: ${homePic}, 
-    linear-gradient(to right, rgba(0,0,0,.5), rgba(0,0,0,.2));
-  height: 80%;
+const Landing = styled.header`
+  align-items: center;
+  background: linear-gradient(to bottom, rgba(0,0,0,.8), rgba(0,0,0,.5)), url(${homePic});
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 80vh;
+  overflow: hidden;
   width: 100%;
 `;
+
+const HeadingContainer = styled.section`
+  background-color: rgba(255, 255, 255, .5);
+  border: 1px solid transparent;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+`;
+
+const Heading = styled.h1`
+  color: whitesmoke;
+  font-size: 3rem;
+  text-shadow: 2px 5px 10px rgba(0,0,0,.9);
+
+  @media (max-width: 500px) {
+    font-size: 2rem;
+  }
+`;
+
+const SubHeading = styled.h2`
+  color: whitesmoke;
+  font-size: 1.8rem;
+  font-weight: 300;
+  text-shadow: 2px 3px 5px rgba(0,0,0,.9);
+
+  @media (max-width: 500px) {
+    font-size: 1.2rem;
+  }
+`;
+
 
 export default function Home() {
   return (
     <Landing>
-      <p>Hi</p>
+      <HeadingContainer>
+        <Heading>Keyboard Group Buy</Heading>
+        <SubHeading>Never miss another group buy!</SubHeading>
+      </HeadingContainer>
     </Landing>
   )
 }
